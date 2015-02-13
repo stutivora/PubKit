96934b2eb567b0dd80fb7feca9de7ce1f01053a5
Puran Singh <mailtopuran@gmail.com>
2015-02-13 13:51:24 -0800

    first commit

---
+++ Server/src/main/java/com/roquito/platform/RoquitoApplication.java
@@ -0,0 +1,12 @@
+package com.roquito.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoquitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoquitoApplication.class, args);
+    }
+}
---
+++ Server/src/test/java/com/roquito/platform/RoquitoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.roquito.platform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RoquitoApplication.class)
+@WebAppConfiguration
+public class RoquitoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
