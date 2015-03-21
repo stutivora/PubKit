8c0bec3c8e40a6add29c77dd3b48378eda6052cf
Puran Singh <mailtopuran@gmail.com>
2015-03-20 22:56:08 -0700

    d

--- Server/src/main/java/com/roquito/RoquitoConfig.java
+++ Server/src/main/java/com/roquito/RoquitoConfig.java
@@ -22,14 +22,12 @@ package com.roquito;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by puran
  */
-@Configuration
 @Component
 public class RoquitoConfig {
     
